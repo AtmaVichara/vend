a72d0ff14f743bce25a915ea09474cd7b8eaa54e
Joe Jobes <jmrjobes@gmail.com>
2018-02-16 09:30:36 -0700

    Add snack model test for relationships

--- spec/features/user_sees_all_snacks_in_vending_machine_spec.rb
+++ spec/features/user_sees_all_snacks_in_vending_machine_spec.rb
@@ -7,14 +7,14 @@ describe "a user sees all snacks and prices" do
       machine = create(:machine, owner_id: owner.id)
       snack = create_list(:snack, 3, machine_id: machine.id)
 
-      visit owner_machine_path(machine)
+      visit machine_path(machine)
 
-      expect(page).to have(snack[0].name)
-      expect(page).to have(snack[0].price)
-      expect(page).to have(snack[1].name)
-      expect(page).to have(snack[1].price)
-      expect(page).to have(snack[2].name)
-      expect(page).to have(snack[2].price)
+      expect(page).to have_content(snack[0].name)
+      expect(page).to have_content(snack[0].price)
+      expect(page).to have_content(snack[1].name)
+      expect(page).to have_content(snack[1].price)
+      expect(page).to have_content(snack[2].name)
+      expect(page).to have_content(snack[2].price)
     end
   end
 end
